394afa8c1f910446f416a813ebe73a1d6d2aa91e
dothinking <train8808@gmail.com>
2021-05-28 18:54:17 +0800

    enhancement: detect text-style-stroke

--- pdf2docx/shape/Shapes.py
+++ pdf2docx/shape/Shapes.py
@@ -155,7 +155,8 @@ class Shapes(ElementCollection):
             Stroke shapes are grouped on connectivity to each other, but in some cases, 
             the gap between borders and underlines/strikes are very close, which leads
             to an incorrect table structure. So, it's required to distinguish them in
-            advance, though we needn't to ensure 100% accuracy.
+            advance, though we needn't to ensure 100% accuracy. They are finally determined 
+            when parsing table structure and text format.
         '''
         # reset all
         self._table_strokes.reset()
@@ -169,9 +170,7 @@ class Shapes(ElementCollection):
 
         # check positions between shapes and text blocks
         for shape in self._instances:
-            # try to determin shape semantic type:
-            # - check if text underline/strike for a stroke
-            # - check if table shading for a fill
+            # try to determin shape semantic type
             rect_type = shape.semantic_type(blocks.text_blocks)     # type: RectType
 
             # set the type if succeeded
@@ -180,25 +179,16 @@ class Shapes(ElementCollection):
 
             elif rect_type==RectType.UNDERLINE_OR_STRIKE:
                 self._text_underlines_strikes.append(shape)
-            
-            elif rect_type==RectType.BORDER:
-                self._table_strokes.append(shape)
 
             elif rect_type==RectType.SHADING:
                 self._table_fillings.append(shape)
-            
-            elif rect_type==RectType.HIGHLIGHT:
-                self._text_highlights.append(shape)
 
-            # if not determined, it should be the opposite type, e.g. table border for a Stroke, 
-            # highlight for a Fill. However, condering margin, incorrectly organized blocks, e.g.
-            # a text underline may have no intersection with the text block, so let's add the shape 
-            # to both groups for conservation. It'll finally determined when parsing table structure
-            # and text format.
+            # otherwise, it should be the opposite type, e.g. table border for a Stroke, 
+            # highlight for a Fill. 
             else:
                 if isinstance(shape, Stroke):
                     self._table_strokes.append(shape)
-                    self._text_underlines_strikes.append(shape)
+                
                 else:
                     self._text_highlights.append(shape)
                     self._table_fillings.append(shape)
@@ -245,15 +235,15 @@ class Shapes(ElementCollection):
 
 
     def plot(self, page):
-        '''Plot shapes for debug purpose.
+        '''Plot shapes for debug purpose. Different colors are used to display the shapes in detected 
+        semantic types, e.g. yellow for text based shape (stroke, underline and highlight). Due to 
+        overlaps between Stroke and Fill related groups, some shapes are plot twice.
         
         Args:
             page (fitz.Page): pdf page.
         '''
-        # different colors are used to show the shapes in detected semantic types
-        # Due to overlaps between Stroke and Fill related groups, some shapes are plot twice.
-
-        # -table shading
+        # Table based shapes
+        # - table shading
         color = (152/255, 251/255, 152/255)
         for shape in self._table_fillings: shape.plot(page, color)
 
@@ -261,10 +251,11 @@ class Shapes(ElementCollection):
         color = (0, 0, 0)
         for shape in self._table_strokes: shape.plot(page, color)
 
+        # Text based shapes
         # - underline and strike-through
-        color = (1, 0, 0)
+        color = (1, 1, 0)
         for shape in self._text_underlines_strikes: shape.plot(page, color)
 
-        # highlight
+        # - highlight
         color = (1, 1, 0)
         for shape in self._text_highlights: shape.plot(page, color)
