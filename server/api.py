17d03f37dd2a662054bee8364d19835c50365149
zhangxiaojia002 <zhangxiaojia002@ke.com>
2023-12-06 11:45:31 +0800

    添加健康检查端口

--- server/api.py
+++ server/api.py
@@ -4,6 +4,17 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/actuator/health/liveness")
+async def health_liveness():
+    return {"status": "UP"}
+
+
+@app.get("/actuator/health/readiness")
+async def health_readiness():
+    return {"status": "UP"}
