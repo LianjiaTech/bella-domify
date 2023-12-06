b8a48ab8a9725948b09e86a4fe0486bab3b36fe6
zhangxiaojia002 <zhangxiaojia002@ke.com>
2023-12-06 12:11:59 +0800

    pdf文件解析测试接口

--- server/api.py
+++ server/api.py
@@ -1,6 +1,9 @@
 # fastapi定义接口
 
 from fastapi import FastAPI
+from fastapi import File, UploadFile
+
+from pdf2docx import Converter
 
 app = FastAPI()
 
@@ -18,3 +21,21 @@ async def health_liveness():
 @app.get("/actuator/health/readiness")
 async def health_readiness():
     return {"status": "UP"}
+
+# 上传文件接口
+@app.post("/pdf/parse/test")
+async def create_upload_file(file: UploadFile = File(...)):
+    # 读取file字节流
+    contents = await file.read()
+    converter = Converter(stream=contents)
+    tables = converter.extract_tables(
+        start=0, end=1,
+        # extract_table_with_cell_pos=True,
+        # remove_watermark=True,
+        # debug=True,
+        # debug_file_name=test_dir + "frt-debug.pdf",
+        sematic_parse=True,
+        # parse_stream_table=False
+    )
+
+    return {"filename": file.filename}
