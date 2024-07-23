9b0dfb983dd88a4854152623decc3e023717a726
luxu002 <luxu002@ke.com>
2024-07-23 15:28:02 +0800

    接口path批量修改

--- server/api.py
+++ server/api.py
@@ -2,41 +2,39 @@
 import logging
 from threading import Thread
 
-import uvicorn
-
-from services import layout_parse, domtree_parse
-from .task_executor.task_manager import create_pdf_parse_task
-from .task_executor.executor import execute_parse_task
-
-from fastapi import FastAPI, Form, Path, Body
+from fastapi import FastAPI, APIRouter
 from fastapi import File, UploadFile
+from fastapi import Form, Path, Body
 
-from pdf2docx import Converter
-from pdf2docx.dom_tree.domtree import DomTreeModel
 from server.log.log_config import log_config
+from services import layout_parse, domtree_parse
+from .task_executor.executor import execute_parse_task
+from .task_executor.task_manager import create_pdf_parse_task
 
 log_config()  # 初始化日志配置
 
 app = FastAPI()
 
+router = APIRouter(prefix="/v1/parser")
 
-@app.get("/")
+
+@router.get("/")
 async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/actuator/health/liveness")
+@router.get("/actuator/health/liveness")
 async def health_liveness():
     return {"status": "UP"}
 
 
-@app.get("/actuator/health/readiness")
+@router.get("/actuator/health/readiness")
 async def health_readiness():
     return {"status": "UP"}
 
 
 # 上传文件接口
-@app.post("/pdf/parse/test")
+@router.post("/pdf/parse/test")
 async def create_upload_file(file: UploadFile = File(...)):
     # 读取file字节流
     contents = await file.read()
@@ -44,32 +42,31 @@ async def create_upload_file(file: UploadFile = File(...)):
 
 
 # 文件解析-获取版面信息
-@app.post("/document/layout/parse")
+@router.post("/document/layout/parse")
 async def create_upload_file(file_name: str = Form(...), file_url_object: UploadFile = File(...)):
     # 读取file字节流
     contents = await file_url_object.read()
     return layout_parse.parse(file_name, contents)
 
 
-@app.post("/async/pdf/parse")
+@router.post("/async/pdf/parse")
 async def async_parse(file: UploadFile = File(...), callback_url: str = Form(...)):
     # 读取file字节流
     return create_pdf_parse_task(file, callback_url)
 
 
 # 定义回调接口，url中包含pathvariable
-@app.post("/async/pdf/parse/callback/{taskNo}")
+@router.post("/async/pdf/parse/callback/{taskNo}")
 async def async_parse_callback(taskNo: str = Path(..., title="The task number"), task: dict = Body(...)):
     logging.info("接收回调")
     print(taskNo, task)
 
 
-@app.on_event("startup")
+@router.on_event("startup")
 async def startup_event():
     print("Starting background task...")
     thread = Thread(target=execute_parse_task)
     thread.start()
-
 
 # if __name__ == "__main__":
 #     uvicorn.run("server.api:app", host="127.0.0.1", port=8080, reload=True)
