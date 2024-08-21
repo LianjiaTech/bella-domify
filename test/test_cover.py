c956b88d4b13fd7185f6ec9f9a3be5049caab16e
liumin01 <liumin01@ke.com>
2024-08-21 20:53:42 +0800

    [紧急] 新增 cover 测试

--- test/test_cover.py
+++ test/test_cover.py
@@ -2,13 +2,14 @@
 # 流程文档
 # https://doc.weixin.qq.com/doc/w3_AOsASwZXANEuo8cdrbvS16KPAnJCD?scode=AJMA1Qc4AAw6bV1mLrAOsASwZXANE
 # ===========================
+import json
 import os
 
 from pdf2docx import Converter
 from pdf2docx.dom_tree.domtree import DomTreeModel
 from server.context import user_context
 
-os.environ["OPENAI_API_KEY"] = "<ak>"
+os.environ["OPENAI_API_KEY"] = "qaekDD2hBoZE4ArZZlOQ9fYTQ74Qc8mq"
 os.environ["OPENAI_BASE_URL"] = "https://openapi-ait.ke.com/v1/"
 
 user_context.set("1000000020353701")
@@ -16,15 +17,18 @@ user_context.set("1000000020353701")
 test_dir = "../test_document/"
 
 
-def pdf_parser(file_name: str) -> dict:
+def pdf_parser(file_name: str, debug: bool = False) -> dict:
     converter = Converter(test_dir + f"{file_name}.pdf")
     dom_tree = converter.dom_tree_parse(
         start=0, end=4,
         remove_watermark=True,
-        debug=False,
-        debug_file_name="",
+        debug=debug,
+        debug_file_name=test_dir + f"{file_name}-debug.pdf",
         parse_stream_table=False
     )
+    if debug:
+        with open(test_dir + f"{file_name}-debug.json", "w") as fw:
+            json.dump(DomTreeModel(dom_tree=dom_tree).model_dump(), fw, ensure_ascii=False, indent=2)
     return DomTreeModel(dom_tree=dom_tree).model_dump()
 
 
@@ -39,6 +43,10 @@ def test_cover():
     assert result4["root"]["child"][0]["element"]["text"].strip() == "新零售品类销售签约规范"
     result5 = pdf_parser("FAQ判断case")
     assert result5["root"]["child"][0]["element"]["text"].strip() in ["明白了，以下是整理后的文档:", "### 营销"]
+    result6 = pdf_parser("奥丁QA")
+    assert result6["root"]["child"][0]["element"]["text"].strip().startswith("Q:")
+    result7 = pdf_parser("《贝壳入职管理制度》5页")
+    assert result7["root"]["child"][0]["element"]["text"].strip() == "为了规范公司新员工的入职管理，明确入职各环节的操作流程的标准化，提高入职手续办理 效率，提供良好的入职办理体验，人力资源共享服务中心特制定本制度，本制度于 2020 年 8 月 11 日生效。"
 
 
 if __name__ == "__main__":
