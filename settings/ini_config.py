7b424bfcb17f346ccd81690bd2522d29357a6f2f
wangyukun017 <wangyukun017@ke.com>
2025-07-04 16:20:46 +0800

    修改review问题

--- settings/ini_config.py
+++ settings/ini_config.py
@@ -2,16 +2,17 @@
 import configparser
 import os
 
+from settings import settings_path
 from utils.tokens_util import init_tiktoken
 
 config = configparser.ConfigParser()
 
-def init_config(base_dir):
+def init_config(base_dir=settings_path):
     env = os.getenv("ENVTYPE", "test")
     # Read local file `settings.ini`.
     if env == "test":
         config.read(os.path.join(base_dir, "test.ini"))
     else:
         config.read(os.path.join(base_dir, "prod.ini"))
-
+init_config()
 init_tiktoken()
